88d6ace1cec463524e0efce5ffc8b501f3fb620a
Igor Kazakov <ikazakof@mail.ru>
2022-10-31 11:35:56 +0300

    Localized documents

--- src/main/java/com/example/MyBookShopApp/controllers/AboutController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/AboutController.java
@@ -1,10 +1,23 @@
 package com.example.MyBookShopApp.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
 
 @Controller
 public class AboutController {
+
+//    @Value("#{'${about.text}'.split(';')}")
+//    private  List<String> about;
+//
+//    @ModelAttribute("aboutText")
+//    private List<String> getAbout() {
+//        return about;
+//    }
+
 
     @GetMapping("/about")
     public String recentPage(){
