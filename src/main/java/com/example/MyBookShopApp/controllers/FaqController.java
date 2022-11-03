1f522bb5ef3fb2cf94c625d0a00e2a5c6b798414
Igor Kazakov <ikazakof@mail.ru>
2022-11-03 16:04:23 +0300

    Localized about & help

--- src/main/java/com/example/MyBookShopApp/controllers/FaqController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/FaqController.java
@@ -1,25 +1,14 @@
 package com.example.MyBookShopApp.controllers;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-
-import java.util.Map;
 
 @Controller
 public class FaqController {
 
-    @Value("#{${help.tips}}")
-    private Map<String,String> valueMap;
-
-    @ModelAttribute("tips")
-    private Map<String, String> getTips() {
-        return valueMap;
-    }
 
     @GetMapping("/faq")
-    public String recentPage(){
+    public String recentPage() {
         return "faq";
     }
 
